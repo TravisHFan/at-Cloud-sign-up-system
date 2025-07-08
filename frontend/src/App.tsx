1a96fb527f295a385316094555d424ece637ed06
TravisHFan <travisfanht@gmail.com>
2025-07-07 17:52:35 -0700

    Update routing to include all the new pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,61 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignUp from './pages/SignUp'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import DashboardLayout from './layouts/DashboardLayout'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import UpcomingEvents from "./pages/UpcomingEvents";
+import PassedEvents from "./pages/PassedEvents";
+import NewEvent from "./pages/NewEvent";
+import Management from "./pages/Management";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard/*" element={
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
           <DashboardLayout>
             <Dashboard />
           </DashboardLayout>
-        } />
-      </Routes>
-    </div>
+        }
+      />
+      <Route
+        path="/dashboard/upcoming"
+        element={
+          <DashboardLayout>
+            <UpcomingEvents />
+          </DashboardLayout>
+        }
+      />
+      <Route
+        path="/dashboard/passed"
+        element={
+          <DashboardLayout>
+            <PassedEvents />
+          </DashboardLayout>
+        }
+      />
+      <Route
+        path="/dashboard/new-event"
+        element={
+          <DashboardLayout>
+            <NewEvent />
+          </DashboardLayout>
+        }
+      />
+      <Route
+        path="/dashboard/management"
+        element={
+          <DashboardLayout>
+            <Management />
+          </DashboardLayout>
+        }
+      />
+    </Routes>
   );
 }
 
