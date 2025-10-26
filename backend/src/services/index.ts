cb59b4483ffd5db3ac2f8a5bfbd6186b1188e5bf
TravisHFan <travisfanht@gmail.com>
2025-10-25 23:29:04 -0700

    fix: remove non-existent service exports from index.ts
    Removed exports for services that don't exist:
    - NotificationService (only TrioNotificationService exists)
    - EventService (doesn't exist)
    - UserService (doesn't exist)
    - SubscriptionService (doesn't exist)
    - LockService from infrastructure (it's in services/LockService.ts)
    - ICSBuilder (not a named export)
    - buildEventICS (doesn't exist, should be buildRegistrationICS)
    - SystemConfigService (doesn't exist)
    
    Fixed all 8 TypeScript compilation errors in services/index.ts

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -5,15 +5,9 @@
  */
 
 // Infrastructure services
-export { NotificationService } from "./notifications/NotificationService";
 export { TrioNotificationService } from "./notifications/TrioNotificationService";
-export { EventService } from "./eventService";
-export { UserService } from "./userService";
-export { SubscriptionService } from "./SubscriptionService";
-export { LockService } from "./infrastructure/LockService";
 export { EmailService } from "./infrastructure/EmailServiceFacade";
-export { ICSBuilder, buildEventICS } from "./ICSBuilder";
-export { SystemConfigService } from "./SystemConfigService";
+export { buildRegistrationICS } from "./ICSBuilder";
 export {
   CacheService,
   cacheService,
